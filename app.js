dd6bf2706273ea7cbe337a9d042e62cb57a6339f
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-04 11:47:47 +0330

    Set up Express app and server configuration
    - Created app.js file as main server entry point
    - Configured Express, CORS, and Morgan middlewares
    - Added default route for testing

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ limits: "30mb", extended: true }));
+app.use(express.json({ limits: "30mb" }));
+// app.use(setHeaders);
+
+// Routers
+
+
+
+app.use((req,res) => {
+    console.log("Thise path is not found:", req.path);
+
+    return res.status(404).json({
+        message: "404! path not found.please double check the path / method",
+    });
+});
+
+app.use(errorHandler);
+
+module.exports = app;
