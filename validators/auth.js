66d374689d7bdde7df188bd4741b2de9bc24cdde
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-04 13:13:20 +0330

    create response helper and validator auth

---
+++ validators/auth.js
@@ -0,0 +1,32 @@
+const yup = require('yup');
+
+const registerValidator = yup.object({
+    email: yup
+    .string()
+    .required("email is required")
+    .email('Email must be a valid email address'),
+    password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+const loginValidator = yup.object({
+    login: yup
+    .string()
+    .required('Username or email is required')
+    .test('is-valid', 'Must be a valid email or username', value => {
+      if (!value) return false;
+
+      const isEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(value);
+      const isUsername = value.length >= 3;
+
+      return isEmail || isUsername;
+    }),
+  
+  password: yup
+    .string()
+    .required('Password is required')
+});
+
+module.exports = { registerValidator, loginValidator };
