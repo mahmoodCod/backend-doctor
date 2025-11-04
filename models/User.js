b9206b853a42d99342da32b2545a681480d858e8
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-04 15:17:04 +0330

    update user model

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
-});
+}, { timestamps: true });
 
 const model = mongoose.model('User', userSchema);
 
