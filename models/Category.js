ad63c034ceaa06ea842a5bca7090c24b736ac558
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-05 16:00:57 +0330

    create model category

---
+++ models/Category.js
@@ -0,0 +1,64 @@
+
+const mongoose = require('mongoose');
+
+const filterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    type: {
+        type: String,
+        enum: [ "radio", "selectbox" ],
+        required: true,
+    },
+    options: {
+        type: [String],
+        validate: {
+            validator: (value) => Array.isArray(value),
+            message: 'Options must be an array of strings',
+        },
+    },
+    min: Number,
+    max: Number,
+});
+
+const categorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    parent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        default: null,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    icon: {
+        filename: { type: String, trim: true },
+        path: { type: String, trim: true },
+    },
+    fillters: [filterSchema],
+}, { timestamps: true });
+
+const model = mongoose.model("Category", categorySchema);
+module.exports = model;
