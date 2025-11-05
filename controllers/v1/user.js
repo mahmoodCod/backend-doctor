f5e5b7bce41cd6bc1271dd71d9413f461f7922a8
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-05 12:41:59 +0330

    create banUser controller

--- controllers/v1/user.js
+++ controllers/v1/user.js
@@ -1,6 +1,7 @@
-const { successResponse } = require('../../helpers/response');
+const { successResponse, errorResponse } = require('../../helpers/response');
 const User = require('../../models/User');
 const { createPaginationData } = require('../../utils');
+const Ban = require('../../models/Ban');
 exports.getAll = async(req,res,next) => {
     try {
         let { page = 1, limit = 10 } = req.query;
@@ -36,6 +37,25 @@ exports.changeRole = async(req,res,next) => {
 
 exports.banUser = async(req,res,next) => {
     try {
+        const { userId } = req.params;
+
+        const user = await User.findOne({ _id: userId });
+        if (!user) {
+            return errorResponse(res,404, "User not found");
+        };
+
+        if (user.roles.includes("ADMIN")) {
+            return errorResponse(res, 403, "You cannot ban an admin !!");
+        };
+
+        const deleteUser = await User.findOneAndDelete({ _id: userId });
+        
+        await Ban.create({ email: user.email });
+
+        return successResponse(res,200, {
+            user : deleteUser,
+            message: "User banned successfully, user and posts removed",
+        });
 
     } catch (err) {
         next(err);
