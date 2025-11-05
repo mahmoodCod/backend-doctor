f9c4d15037d02661d42f83f12669ed95b9f7cb43
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-05 16:33:10 +0330

    update router user

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { auth } = require('../../middlewares/auth');
 const roleGuard = require('../../middlewares/roleGuard');
-const { getAll, updateUser, changeRole, banUser } = require('../../controllers/v1/user');
+const { getAll, updateUser, changeRole, banUser, getDoctors, updateDoctorInfo } = require('../../controllers/v1/user');
 
 const router = express.Router();
 
 router.route('/').get(auth, roleGuard('ADMIN'), getAll)
 .patch(auth, updateUser);
+router.get('/doctors', getDoctors);
+router.patch('/doctor-info', auth, roleGuard('DOCTOR'), updateDoctorInfo);
 router.route('/role/:userId').put(auth, roleGuard('ADMIN'), changeRole);
 router.route('/ban/:userId').post(auth, roleGuard('ADMIN'), banUser);
 
