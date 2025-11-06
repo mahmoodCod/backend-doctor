fa777e2b7908c81969220bcc55d974734513effc
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-06 22:23:16 +0330

    update model user doctorInfoSchema

--- models/User.js
+++ models/User.js
@@ -5,27 +5,33 @@ const doctorInfoSchema = new mongoose.Schema({
     fullname: {
         type: String,
         trim: true,
+        required: true,
     },
     slug: {
         type: String,
         trim: true,
         unique: true,
+        required: true,
     },
     phone: {
         type: String,
         trim: true,
+        required: true,
     },
     address: { 
         type: String,
-        trim: true, 
+        trim: true,
+        required: true, 
     },
     city: {
         type: String, 
         trim: true,
+        required: true,
     },
     province: { 
         type: String, 
         trim: true,
+        required: true,
     },
     bio: { 
         type: String, 
@@ -33,15 +39,18 @@ const doctorInfoSchema = new mongoose.Schema({
     },
     experience: { 
         type: Number, 
-        default: 0, 
+        default: 0,
+        required: true, 
     },
     price: { 
         type: Number, 
-        default: 0, 
+        default: 0,
+        required: true, 
     },
     rating: { 
         type: Number, 
-        default: 0, 
+        default: 0,
+        required: true, 
     },
     visitStatus: { 
         type: Boolean, 
@@ -50,17 +59,18 @@ const doctorInfoSchema = new mongoose.Schema({
     category: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Category',
+        required: true,
     },
     avatar: {
-      filename: { type: String },
-      path: { type: String },
+      filename: { type: String, },
+      path: { type: String, },
     },
     workTimes: {
       type: [
         {
-          day: { type: String },
-          from: { type: String },
-          to: { type: String },
+          day: { type: String, },
+          from: { type: String, },
+          to: { type: String, },
         },
       ],
       default: [],
