54c5d637ff9b05fad6cae5b68d5d7c3543da473c
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-07 11:31:28 +0330

    update router category

--- routes/v1/Category.js
+++ routes/v1/Category.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
 const roleGuard = require('../../middlewares/roleGuard');
+const { multerStorage } = require('../../utils/multerConfigs');
+
+const upload = multerStorage('public/images/category-icons');
 
 const router = express.Router();
 
-router.route('/').post(auth, roleGuard('ADMIN'), createCategory)
+router.route('/').post(auth, roleGuard('ADMIN', upload.single('icon')), createCategory)
 .get(auth, roleGuard('ADMIN'), getAll);
 
-router.route('/category/:categoryId').patch(auth, roleGuard('ADMIN'), updateCategory)
+router.route('/category/:categoryId').patch(auth, roleGuard('ADMIN', upload.single('icon')), updateCategory)
 remove(auth, roleGuard('ADMIN'), removeCategory);
 
 module.exports = router;
